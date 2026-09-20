c4f6998b36096c0ff03f65fb77f24e0c78c7f3c6
Arjel buzi <arjel.buzi1@gmail.com>
2026-09-20 15:51:39 +0200

    created the basic

---
+++ lib/models/video_model.dart
@@ -0,0 +1,43 @@
+class AudioTrack {
+  final String language; // "it", "en"
+  final String code; // "it", "en" (per le query)
+
+  AudioTrack({required this.language, required this.code});
+}
+
+class Episode {
+  final int number;
+  final int season;
+  final String title;
+
+  Episode({required this.number, required this.season, required this.title});
+}
+
+class Video {
+  final int id;
+  final String title;
+  final String type; // "movie" o "series"
+  final List<AudioTrack> audioTracks;
+  final Episode? currentEpisode; // null se è un film
+  final Episode? nextEpisode; // null se è film o ultimo episodio
+  final int duration;
+
+  Video({
+    required this.id,
+    required this.title,
+    required this.type,
+    required this.audioTracks,
+    this.currentEpisode,
+    this.nextEpisode,
+    required this.duration,
+  });
+
+  // Metodo helper: costruisci l'URL dello stream
+  String getStreamUrl(String language) {
+    if (type == "movie") {
+      return "http://localhost:5555/movie/$id/?lang=$language";
+    } else {
+      return "http://localhost:5555/tv/$id/${currentEpisode?.season}/${currentEpisode?.number}/?lang=$language";
+    }
+  }
+}
