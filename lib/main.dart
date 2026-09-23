588f37569030ae28a2e7e65d193d55eec666c8c1
Arjel buzi <arjel.buzi1@gmail.com>
2026-09-24 00:31:37 +0200

    fuck you flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:media_kit/media_kit.dart';
 import 'package:onlystream/screens/video_player_screen.dart';
 import 'package:onlystream/test/test_player.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
