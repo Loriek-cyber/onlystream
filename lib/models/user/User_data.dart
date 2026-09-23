775ddf44a483d6945a47381c8993533176673653
Arjel buzi <arjel.buzi1@gmail.com>
2026-09-23 23:41:14 +0200

    added the first interaction of user_data & modified android_manifest to add the right permissions

---
+++ lib/models/user/User_data.dart
@@ -0,0 +1,7 @@
+import 'dart:ffi';
+
+class UserData {
+  final Long id;
+  final String username;
+  UserData({required this.id, required this.username});
+}
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,3 +1,3 @@
 class AppConstants {
-  static const String baseUrl = "http://locahost:5555";
+  static const String baseUrl = "http://localhost:5555";
 }
